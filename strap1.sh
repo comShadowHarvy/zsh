1db268fdc62731faed444b8388607b21e135a9fc
ShadowHarvy <jimbob343@gmail.com>
2020-04-01 08:24:17 -0400

    updated setup

--- strap1.sh
+++ strap1.sh
@@ -1,1 +1,15 @@
-strap2.sh
+#!/bin/sh
+# strap1.sh - prep my system
+
+echo "trizen for AUR support"
+sudo pacman -S trizen 
+trizen -S tizonia spotifyd iris sconsify mopidy-spotify ncureses mopidy-local ytmdl dnd-tools subtitles-printer-git netflix-qdesktop silos hwinfo powerpill opencl-nvidia-tkg snapd screenfetch neofetch zsh snapd yadm screenfetch neofetch emacs nano
+echo "Enabling snapd support"
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+echo "DOOM emacs"
+git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
+sudo snap install skype --classic
+sudo snap install snap-store
+sudo strap.sh
