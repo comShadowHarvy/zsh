a7f7fbad81ccaffd4f7e25471c7152a34121108f
jimbob343 <jimbob343@gmail.com>
2020-04-16 01:35:30 +0000

    Update strap1.sh

--- strap1.sh
+++ strap1.sh
@@ -1,9 +1,91 @@
 #!/bin/sh
 # strap1.sh - prep my system
+######CHAOTIC AUR Support
+echo "CHAOTIC AUR Support"
+sudo cat >> "/etc/pacman.conf" << EOF
+[chaotic-aur]
+Server = http://lonewolf-builder.duckdns.org/$repo/x86_64
+Server = http://chaotic.bangl.de/$repo/x86_64
+Server = https://repo.kitsuna.net/x86_64
+#########################Must add keys for it
+#sudo pacman-key --keyserver keys.mozilla.org -r 3056513887B78AEB
+#sudo pacman-key --lsign-key 3056513887B78AEB
+EOF
+
+sudo pacman-key --keyserver keys.mozilla.org -r 3056513887B78AEB
+sudo pacman-key --lsign-key 3056513887B78AEB
+
 
 echo "trizen for AUR support"
 sudo pacman -S trizen zsh
-trizen -S tizonia git spotifyd iris sconsify mopidy-spotify ncureses mopidy-local ytmdl dnd-tools subtitles-printer-git netflix-qdesktop silos hwinfo powerpill opencl-nvidia-tkg snapd screenfetch neofetch pfetch archey4 zsh snapd yadm screenfetch neofetch emacs nano git-summary nerd-fonts-complete nerd-fonts-hack vdhcoapp atom-transparent code-transparent lib32-gtk3 lib32-libxslt ccache schedtool lib32-v4l-utils ghc cabal-install zfz a2sv aphopper apkstat apt2 auto-auto-complete biglybt-extreme-mod tldr alacritty lazygit terminus edex-ui npm no-more-secrets-git spotify-tui rtv mps-youtube-hd-git navi wmctrl virt-what pciutils lm_sensors bind-tools lolcat
+trizen -S tizonia 
+trizen -S git 
+trizen -S spotifyd 
+trizen -S iris 
+trizen -S sconsify 
+trizen -S mopidy-spotify 
+trizen -S ncureses 
+trizen -S mopidy-local 
+trizen -S ytmdl 
+trizen -S dnd-tools 
+trizen -S subtitles-printer-git 
+trizen -S netflix-qdesktop 
+trizen -S silos 
+trizen -S hwinfo 
+trizen -S powerpill 
+trizen -S opencl-nvidia-tkg 
+trizen -S snapd 
+trizen -S screenfetch 
+trizen -S neofetch 
+trizen -S pfetch 
+trizen -S archey4 
+trizen -S snapd 
+echo "Enabling snapd support"
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+trizen -S yadm
+yadm clone http://github.com/jimbob343/zsh
+trizen -S screenfetch 
+trizen -S neofetch 
+trizen -S emacs 
+trizen -S nano 
+trizen -S git-summary 
+trizen -S nerd-fonts-complete 
+trizen -S nerd-fonts-hack 
+trizen -S vdhcoapp 
+trizen -S atom-transparent 
+trizen -S code-transparent 
+trizen -S lib32-gtk3 
+trizen -S lib32-libxslt 
+trizen -S ccache 
+trizen -S schedtool 
+trizen -S lib32-v4l-utils 
+trizen -S ghc 
+trizen -S cabal-install 
+trizen -S zfz 
+trizen -S a2sv 
+trizen -S aphopper 
+trizen -S apkstat 
+trizen -S apt2 
+trizen -S auto-auto-complete 
+trizen -S biglybt-extreme-mod 
+trizen -S tldr 
+trizen -S alacritty 
+trizen -S lazygit 
+trizen -S terminus 
+trizen -S edex-ui 
+trizen -S npm 
+trizen -S no-more-secrets-git 
+trizen -S spotify-tui 
+trizen -S rtv 
+trizen -S mps-youtube-hd-git 
+trizen -S navi 
+trizen -S wmctrl 
+trizen -S virt-what 
+trizen -S pciutils 
+trizen -S lm_sensors 
+trizen -S bind-tools 
+trizen -S lolcat
 
 chsh -s =zsh
 git clone https://github.com/gpakosz/.tmux.git
@@ -13,9 +95,9 @@ npm i chalk-animation
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
-echo "Enabling snapd support"
-sudo systemctl enable --now snapd.socket
-sudo ln -s /var/lib/snapd/snap /snap
+#echo "Enabling snapd support"
+#sudo systemctl enable --now snapd.socket
+#sudo ln -s /var/lib/snapd/snap /snap
 echo "yadm pull of .dot files"
 curl -L git.io/antigen > antigen.zsh
 yadm clone https://github.com/jimbob343/zsh
