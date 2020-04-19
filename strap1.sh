1ab6c0db4dec92feeeb59c660b078bb0274ffdb0
ShadowHarvy <jimbob343@gmail.com>
2020-04-19 11:13:46 -0400

    noconferm

--- strap1.sh
+++ strap1.sh
@@ -21,77 +21,77 @@ chmod +x strap.sh
 sudo ./strap.sh
 
 echo "trizen for AUR support"
-sudo pacman -S trizen zsh
-trizen -S tizonia 
-trizen -S git 
-trizen -S spotifyd 
-trizen -S iris 
-trizen -S sconsify 
-trizen -S mopidy-spotify 
-trizen -S ncureses 
-trizen -S mopidy-local 
-trizen -S ytmdl 
-trizen -S dnd-tools 
-trizen -S subtitles-printer-git 
-trizen -S netflix-qdesktop 
-trizen -S silos 
-trizen -S hwinfo 
-trizen -S powerpill 
-trizen -S opencl-nvidia-tkg 
-trizen -S snapd 
-trizen -S screenfetch 
-trizen -S neofetch 
-trizen -S pfetch 
-trizen -S archey4 
-trizen -S snapd 
+sudo pacman -S --noconfirm trizen zsh
+trizen -S --noconfirm tizonia 
+trizen -S --noconfirm git 
+trizen -S --noconfirm spotifyd 
+trizen -S --noconfirm iris 
+trizen -S --noconfirm sconsify 
+trizen -S --noconfirm mopidy-spotify 
+trizen -S --noconfirm ncureses 
+trizen -S --noconfirm mopidy-local 
+trizen -S --noconfirm ytmdl 
+trizen -S --noconfirm dnd-tools 
+trizen -S --noconfirm subtitles-printer-git 
+trizen -S --noconfirm netflix-qdesktop 
+trizen -S --noconfirm silos 
+trizen -S --noconfirm hwinfo 
+trizen -S --noconfirm powerpill 
+trizen -S --noconfirm opencl-nvidia-tkg 
+#trizen -S --noconfirm snapd 
+trizen -S --noconfirm screenfetch 
+trizen -S --noconfirm neofetch 
+trizen -S --noconfirm pfetch 
+trizen -S --noconfirm archey4 
+trizen -S --noconfirm snapd 
 echo "Enabling snapd support"
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
-trizen -S yadm
+trizen -S --noconfirm yadm
 yadm clone http://github.com/jimbob343/zsh
-trizen -S screenfetch 
-trizen -S neofetch 
-trizen -S emacs 
-trizen -S nano 
-trizen -S git-summary 
-trizen -S nerd-fonts-complete 
-trizen -S nerd-fonts-hack 
-trizen -S vdhcoapp 
-trizen -S atom-transparent 
-trizen -S code-transparent 
-trizen -S lib32-gtk3 
-trizen -S lib32-libxslt 
-trizen -S ccache 
-trizen -S schedtool 
-trizen -S lib32-v4l-utils 
-trizen -S ghc 
-trizen -S cabal-install 
-trizen -S zfz 
-trizen -S a2sv 
-trizen -S aphopper 
-trizen -S apkstat 
-trizen -S apt2 
-trizen -S auto-auto-complete 
-trizen -S biglybt-extreme-mod 
-trizen -S tldr 
-trizen -S alacritty 
-trizen -S lazygit 
-trizen -S terminus 
-trizen -S edex-ui 
-trizen -S npm 
-trizen -S no-more-secrets-git 
-trizen -S spotify-tui 
-trizen -S rtv 
-trizen -S mps-youtube-hd-git 
-trizen -S navi 
-trizen -S wmctrl 
-trizen -S virt-what 
-trizen -S pciutils 
-trizen -S lm_sensors 
-trizen -S bind-tools 
-trizen -S lolcat
+trizen -S --noconfirm screenfetch 
+trizen -S --noconfirm neofetch 
+trizen -S --noconfirm emacs 
+trizen -S --noconfirm nano 
+trizen -S --noconfirm git-summary 
+trizen -S --noconfirm nerd-fonts-complete 
+trizen -S --noconfirm nerd-fonts-hack 
+trizen -S --noconfirm vdhcoapp 
+trizen -S --noconfirm atom-transparent 
+trizen -S --noconfirm code-transparent 
+trizen -S --noconfirm lib32-gtk3 
+trizen -S --noconfirm lib32-libxslt 
+trizen -S --noconfirm ccache 
+trizen -S --noconfirm schedtool 
+trizen -S --noconfirm lib32-v4l-utils 
+trizen -S --noconfirm ghc 
+trizen -S --noconfirm cabal-install 
+trizen -S --noconfirm zfz 
+trizen -S --noconfirm a2sv 
+trizen -S --noconfirm aphopper 
+trizen -S --noconfirm apkstat 
+trizen -S --noconfirm apt2 
+trizen -S --noconfirm auto-auto-complete 
+trizen -S --noconfirm biglybt-extreme-mod 
+trizen -S --noconfirm tldr 
+trizen -S --noconfirm alacritty 
+trizen -S --noconfirm lazygit 
+trizen -S --noconfirm terminus 
+trizen -S --noconfirm edex-ui 
+trizen -S --noconfirm npm 
+trizen -S --noconfirm no-more-secrets-git 
+trizen -S --noconfirm spotify-tui 
+trizen -S --noconfirm rtv 
+trizen -S --noconfirm mps-youtube-hd-git 
+trizen -S --noconfirm navi 
+trizen -S --noconfirm wmctrl 
+trizen -S --noconfirm virt-what 
+trizen -S --noconfirm pciutils 
+trizen -S --noconfirm lm_sensors 
+trizen -S --noconfirm bind-tools 
+trizen -S --noconfirm lolcat
 
-chsh -s =zsh
+chsh -s =/bin/zsh
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
