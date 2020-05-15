2e40940673e446e7e5a359db4459fd01260d9ce6
ShadowHarvy <jimbob343@gmail.com>
2020-05-15 18:07:13 -0400

    byobu added

--- strap1.sh
+++ strap1.sh
@@ -50,7 +50,16 @@ echo "DOOM emacs"
 git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
 echo "byobu config"
-export SOMEWHERE=~/.config/byobu; git clone https://github.com/b3rserker/byobu-config $SOMEWHERE; ln -s $SOMEWHERE ~/.byobu
+export SOMEWHERE=~/.config/byobu
+git clone https://github.com/b3rserker/byobu-config $SOMEWHERE
+ln -s $SOMEWHERE ~/.byobu
+export PLUGS=$SOMEWHERE/plugins
+git clone https://github.com/tmux-plugins/tpm $PLUGS/tpm
+git clone https://github.com/tmux-plugins/tmux-sidebar $PLUGS/tmux-sidebar
+git clone https://github.com/tmux-plugins/tmux-copycat $PLUGS/copycat2
+git clone https://github.com/tmux-plugins/tmux-yank $PLUGS/yank
+git clone https://github.com/tmux-plugins/tmux-cpu $PLUGS/tmux-cpu
+git clone https://github.com/tmux-plugins/tmux-resurrect $PLUGS/tmux-resurrect
 trizen -S --noconfirm tizonia 
 trizen -S --noconfirm popcorntime
 trizen -S --noconfirm atom
